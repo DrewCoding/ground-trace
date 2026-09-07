caf1c18ae7192af8ce643e08288cbd2be4ec4294
DrewCoding <drewwilliams9090@gmail.com>
2026-09-07 16:44:57 -0700

    add project name variable

--- infrastructure/aws/variables.tf
+++ infrastructure/aws/variables.tf
@@ -22,3 +22,8 @@ variable "private_subnet_cidrs"{
     type = list(string)
     default = ["10.0.101.0/24", "10.0.102.0/24"]
 }
+
+variable "project_name" {
+    type = string
+    default = "ground-trace"
+}
