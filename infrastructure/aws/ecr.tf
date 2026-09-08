c1cfea9992471a718c07136e2952de38b60e0c01
DrewCoding <drewwilliams9090@gmail.com>
2026-09-07 21:18:30 -0700

    ECR lifecycle policy and output

--- infrastructure/aws/ecr.tf
+++ infrastructure/aws/ecr.tf
@@ -6,3 +6,20 @@ resource "aws_ecr_repository" "game_server"{
         scan_on_push = true
     }
 }
+
+resource "aws_ecr_lifecycle_policy" "game_server"{
+    repository = aws_ecr_repository.game_server.name
+
+    policy = jsonencode({
+        rulePriority = 1
+        description  = "Keep only the 5 most recent images"
+        selection = {
+        tagStatus   = "any"
+        countType   = "imageCountMoreThan"
+        countNumber = 5
+        }
+        action = {
+            type = "expire"
+        }
+    })
+}
