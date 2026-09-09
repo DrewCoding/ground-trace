4de99360d55704bf354e130a54b6246fd9526b06
DrewCoding <drewwilliams9090@gmail.com>
2026-09-08 20:54:59 -0700

    output for session table

--- infrastructure/aws/output.tf
+++ infrastructure/aws/output.tf
@@ -1,3 +1,8 @@
 output "ecr_repository_url"{
     value = aws_ecr_repository.game_server.repository_url
 }
+
+output "sessions_table_name" {
+  value = aws_dynamodb_table.sessions.name
+  description = "Passed to the backend as an env var."
+}
