e1d3545297a2560647fb8de246af40dfaf08f875
DrewCoding <drewwilliams9090@gmail.com>
2026-09-12 20:58:11 -0700

    sights in clusters enabled

--- infrastructure/aws/ecs.tf
+++ infrastructure/aws/ecs.tf
@@ -1,5 +1,10 @@
 resource "aws_ecs_cluster" "main" {
     name = "${var.project_name}-cluster"
+
+    setting {
+        name = "containerInsights"
+        value = "enabled"
+    }
 }
 
 resource "aws_cloudwatch_log_group" "game_server" {
